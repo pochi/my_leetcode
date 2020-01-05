af5af52fede6f64416c5feb5361c3185dae20ceb
pochi <pochi.black@gmail.com>
2020-01-05 12:53:06 +0900

    [leetcode 015] implement binary search tree, but performance still bad

--- 015_three_sum/main.go
+++ 015_three_sum/main.go
@@ -33,24 +33,17 @@ func threeSum(nums []int) [][]int {
 	
 	if len(zeros) >= 1 {
 		for i:=0; i<len(under_zeros); i++ {
-			for j:=0; j<len(over_zeros); j++ {
-				if (under_zeros[i] + over_zeros[j]) == 0 {
-					d = false
-					c = []int{under_zeros[i], 0, over_zeros[j]}
-					for i:=0; i<len(answers); i++ {
-						if reflect.DeepEqual(c, answers[i]) {
-							d = true
-						}
+			if find(over_zeros, under_zeros[i]*-1) {
+				d = false
+				c = []int{under_zeros[i], 0, under_zeros[i]*-1}
+				for i:=0; i<len(answers); i++ {
+					if reflect.DeepEqual(c, answers[i]) {
+						d = true
 					}
-
-					if d == false {
-						answers = append(answers, c)						
-					}
-					break
 				}
 
-				if (under_zeros[i]*-1) < over_zeros[j] {
-					break
+				if d == false {
+					answers = append(answers, c)						
 				}
 			}
 		}
@@ -59,24 +52,18 @@ func threeSum(nums []int) [][]int {
 
 	for i:=0; i<(len(under_zeros)-1); i++ {
 		for j:=i+1; j<len(under_zeros); j++ {
-			for h:=0; h<len(over_zeros); h++ {
-				if under_zeros[i] + under_zeros[j] + over_zeros[h] == 0 {
-					d = false
-					c = []int{under_zeros[i], under_zeros[j], over_zeros[h]}
-					sort.Ints(c)
-					for i:=0; i<len(answers); i++ {
-						if reflect.DeepEqual(c, answers[i]) {
-							d = true
-						}
-					}
-
-					if d == false {
-						answers = append(answers, c)					
+			if find(over_zeros, (under_zeros[i] + under_zeros[j])*-1) {
+				d = false
+				c = []int{under_zeros[i], under_zeros[j], (under_zeros[i] + under_zeros[j])*-1}
+				sort.Ints(c)
+				for i:=0; i<len(answers); i++ {
+					if reflect.DeepEqual(c, answers[i]) {
+						d = true
 					}
 				}
 
-				if ((under_zeros[i]+under_zeros[j])*-1) < over_zeros[h] {
-					break
+				if d == false {
+					answers = append(answers, c)					
 				}
 			}
 		}
@@ -84,29 +71,45 @@ func threeSum(nums []int) [][]int {
 
 	for i:=0; i<(len(over_zeros)-1); i++ {
 		for j:=i+1; j<len(over_zeros); j++ {
-			for h:=0; h<len(under_zeros); h++ {
-				if over_zeros[i] + over_zeros[j] + under_zeros[h] == 0 {
-					d = false
-					c = []int{over_zeros[i], over_zeros[j], under_zeros[h]}
-					sort.Ints(c)
-					for i:=0; i<len(answers); i++ {
-						if reflect.DeepEqual(c, answers[i]) {
-							d = true
-						}
+			if find(under_zeros, (over_zeros[i] + over_zeros[j])*-1) {
+				d = false
+				c = []int{over_zeros[i], over_zeros[j], (over_zeros[i] + over_zeros[j])*-1}
+				sort.Ints(c)
+				for i:=0; i<len(answers); i++ {
+					if reflect.DeepEqual(c, answers[i]) {
+						d = true
 					}
-
-					if d == false {
-						answers = append(answers, c)					
-					}
-					break
 				}
 
-				if (over_zeros[i]+over_zeros[j]) > (under_zeros[h]*-1) {
-					break
+				if d == false {
+					answers = append(answers, c)					
 				}
 			}
 		}
 	}
 
 	return answers
+}
+
+func find(nums []int, t int) bool {
+	l := len(nums)
+    s := 0
+    e := l - 1
+    var i int
+    for {
+        if e < s {
+            return false
+        }
+        i = (s + e) / 2
+
+        if nums[i] == t {
+            return true
+        }
+
+        if nums[i] < t {
+            s = i + 1
+        } else {
+            e = i - 1
+        }
+    }
 }
